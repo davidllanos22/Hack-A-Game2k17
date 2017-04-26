931e5bfe676bc425e0f27b892807061a3929f8c2
ralleg314 <ralleg314@gmail.com>
2017-04-26 18:07:58 +0200

    botones con numeritos sesis

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -48,10 +48,10 @@ class PlayState extends FlxState{
 
 		analog = new FlxAnalog(60, FlxG.height - 60, 50, 0);
 		//button1 = new Button(FlxG.width - 90, FlxG.height - 50, 20, this);
-		button1 = new FlxButton(FlxG.width - 90, FlxG.height - 50);
+		button1 = new FlxButton(FlxG.width - 90, FlxG.height - 50,"1");
 		button1.setSize(50,50);
 		//button2 = new Button(FlxG.width - 50, FlxG.height - 90, 20, this);
-		button2 = new FlxButton(FlxG.width - 50, FlxG.height - 90);
+		button2 = new FlxButton(FlxG.width - 50, FlxG.height - 90,"2");
 		button2.setSize(50,50);
 		inventory = new Inventory();
 
