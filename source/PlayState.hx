79296a2c0cf8e9324613e656da6bc8d98b1f58cf
ralleg314 <ralleg314@gmail.com>
2017-04-26 09:15:49 +0200

    defeatState y muerte del player

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -9,11 +9,13 @@ import flixel.math.FlxMath;
 import flixel.text.FlxText;
 import flixel.math.FlxPoint;
 import flixel.group.FlxSpriteGroup;
+import flixel.FlxCamera;
 
 class PlayState extends FlxState{
 	private var text:FlxText;
 	private var position:FlxPoint = new FlxPoint();
 	private var t:Float = 0;
+	private var player:Player;
 	private var zombis:FlxSpriteGroup;
 	private var obstacles:FlxSpriteGroup;
 	private var bullets:FlxSpriteGroup;
@@ -26,19 +28,20 @@ class PlayState extends FlxState{
 		text.screenCenter();
 		text.getPosition().copyTo(position);
 		add(text);
+		FlxG.camera.follow(player, TOPDOWN, 1);
 	}
 
 	override public function update(elapsed:Float):Void{
 		super.update(elapsed);
-		FlxG.pixelPerfectOverlap(bullets,zombis,hitBullet);
-		FlxG.pixelPerfectOverlap(obstacles,zombis,hitObstacle);
+		FlxG.pixelPerfectOverlap(bullets,zombis,hitBulletZombi);
+		FlxG.pixelPerfectOverlap(obstacles,zombis,hitObstacleZombi);
 		/*t += 0.1;
 		position.add(SPEED, 0);
 		if(position.x > FlxG.camera.width) position.x = - 120;
 		text.setPosition(position.x, position.y + Math.cos(t) * 10);*/
 	}
 
-	private function hitBullet(b:Bullet,z:Zombie){
+	private function hitBulletZombi(b:Bullet,z:Zombie){
 		bullets.remove(b);
 		b.kill();
 
@@ -49,7 +52,7 @@ class PlayState extends FlxState{
 		}
 	}
 
-	private function hitObstacle(o:Obstacle,z:Zombie){
+	private function hitObstacleZombi(o:Obstacle,z:Zombie){
 		o.getHit(z.damage);
 		if(o.life<=0){
 			obstacles.remove(o);
@@ -60,6 +63,13 @@ class PlayState extends FlxState{
 		if(z.life<=0){
 			zombis.remove(z);
 			z.kill();
+		}
+	}
+
+	private function hitPlayerZombi(z:Zombie){
+		player.getHit(z.damage);
+		if(player.life<=0){
+			FlxG.switchState(new DefeatState());
 		}
 	}
 }
