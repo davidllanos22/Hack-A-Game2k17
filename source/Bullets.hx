0872703e8f88dc927f01b0fdaf1bb60505247b2b
MSBeatles <kamehamemarc@gmail.com>
2017-04-26 08:41:09 +0200

    Bullet

---
+++ source/Bullets.hx
@@ -0,0 +1,28 @@
+package;
+import flixel.FlxG;
+import flixel.math.FlxMath;
+import flixel.FlxSprite;
+import flixel.math.FlxAngle;
+import flixel.math.FlxPoint;
+import Math;
+
+class Bullets extends FlxSprite{
+	public var _speed:Float = 400;
+	public var _rAngle:Float;
+
+	public function new (x: Float, y: Float, angle: Float){
+		super(x, y);
+		this._rAngle = angle;
+		//Cargar sprite y animación.
+	}
+
+	private function movement():Void{
+		velocity.set(_speed, 0);
+		velocity.rotate(FlxPoint.weak(0, 0), _rAngle);
+	}
+
+	override public function update(elapsed:Float):Void{
+		movement();
+		super.update(elapsed);	
+	}
+}
