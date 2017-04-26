4f7cfa2b6c024a746c4f0b08eeecaca7b9d1f3ef
ralleg314 <ralleg314@gmail.com>
2017-04-26 08:35:40 +0200

    obstaculo inicial hecho

---
+++ source/Obstacle.hx
@@ -0,0 +1,33 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.util.FlxColor;
+import flixel.math.FlxPoint;
+import flixel.math.FlxRandom;
+
+class Enemy extends FlxSprite{
+	public var life:Int;
+	public var damage:Int;
+
+	public function new(x:Float, y:Float, sizeX:Int, sizeY:Int, life:Int) {
+        super(x, y);
+        setSize(sizeX,sizeY);
+        this.life=life;
+        this.damage=0;
+    }
+
+    public function new(x:Float, y:Float, sizeX:Int, sizeY:Int, life:Int, damage:Int) {
+        super(x, y);
+        setSize(sizeX,sizeY);
+        this.life=life; 
+        this.damage=damage;   
+    }
+
+    override public function update(elapsed:Float):Void {
+        super.update(elapsed);
+    }
+
+    public function getHit(damage:Int){
+    	life-=damage;
+    }
+}
